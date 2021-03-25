52ca34dd468b69f273ad1de584726ae5257b5168
Pau Mezzadra <paumezzadra@hotmail.com>
2021-03-25 14:26:37 -0300

    Add project files.

---
+++ CadenasEjercicio7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadenasEjercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "";
+
+            do
+            {
+                Console.WriteLine("Ingrese su nombre");
+                nombre = Console.ReadLine().ToUpper();
+                if (nombre == "")
+                {
+                    Console.WriteLine("No puede quedar vacío");
+                }
+                else
+                {
+                    Console.WriteLine($"Hola {nombre}");
+                }
+            } while (nombre == "");
+
+            Console.ReadKey();
+
+        }
+    }
+}
